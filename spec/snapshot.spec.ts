4e55b7a8692aae981600f9b22aa52ede3a16c97c
Aki.Yano <orihika0123@gmail.com>
2021-08-15 12:15:28 +0900

    test: modify beforeEach

--- spec/snapshot.spec.ts
+++ spec/snapshot.spec.ts
@@ -3,6 +3,8 @@ import path from "path";
 describe("TODOアプリ", () => {
   beforeEach(async () => {
     await page.goto(`file://${path.resolve(__dirname, "../index.html")}`);
+    await page.waitForSelector("#todoList", { visible: false });
+    await page.waitForSelector("#todoEmpty", { visible: true });
   });
 
   it("初期表示", async () => {
--- spec/visual.spec.ts
+++ spec/visual.spec.ts
@@ -6,6 +6,8 @@ expect.extend({ toMatchImageSnapshot });
 describe("TODOアプリ", () => {
   beforeEach(async () => {
     await page.goto(`file://${path.resolve(__dirname, "../index.html")}`);
+    await page.waitForSelector("#todoList", { visible: false });
+    await page.waitForSelector("#todoEmpty", { visible: true });
   });
 
   it("初期表示", async () => {
