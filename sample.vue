c7597934993c68fbc5421b7947271619ffe7deb0
Aki.Yano <orihika0123@gmail.com>
2021-08-19 22:00:38 +0900

    feat: add vue

---
+++ sample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">sample</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      str: "abc" as String
+    }
+  }
+})
+</script>
+
+
+<style>
+.test {
+  color: black;
+}
+</style>
