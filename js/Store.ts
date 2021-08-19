bdfd665374885b891845da19d862c605ba496582
Aki.Yano <orihika0123@gmail.com>
2021-08-19 23:20:41 +0900

    feat: add todolist component

--- js/Store.ts
+++ js/Store.ts
@@ -1,16 +1,31 @@
 import Vue from "vue";
 
+type Todo = {
+  key: number;
+  todo: string;
+};
+
 const store = Vue.observable({
+  todoList: [] as Todo[],
   nextTodoText: "",
   todoCount: 0,
 });
 
 export const mutations = {
+  addTodo() {
+    store.todoList.push({
+      key: new Date().getTime(),
+      todo: "",
+    });
+  },
+  removeTodo(index: number) {
+    store.todoList.splice(index, 1);
+  },
   updateNextTodoText(nextTodoText: string) {
     store.nextTodoText = nextTodoText;
   },
   updateTodoCount(todoCount: number) {
-    store.todoCount = todoCount
+    store.todoCount = todoCount;
   },
 };
 
